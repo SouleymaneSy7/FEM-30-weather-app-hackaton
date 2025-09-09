3e2de567f28d4fc6ed3e32b852807f4367afa3f0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-09 16:42:30 +0000

    chore(types): add the typescript types

---
+++ src/types/index.d.ts
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export type ContainerTypes<T extends React.ElementType> = {
+  as?: T;
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<T>;
+
+export type ListPropsType<Item, As extends React.ElementType> = {
+  items: Item[];
+  renderItem: (item: Item) => React.ReactNode;
+  as?: As;
+};
+
+export type VisuallyHiddenPropsType = {
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<"span">;
+
+export interface TitleProps extends React.HTMLAttributes<HTMLElement> {
+  level: keyof React.JSX.IntrinsicElements;
+  children: React.ReactNode;
+}
