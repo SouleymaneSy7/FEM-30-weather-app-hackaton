e87825c64c05dbc2d6df05e83f019e155bdd68bb
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-10 01:35:20 +0000

    feat: update the root layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,22 @@ import "./globals.css";
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
   subsets: ["latin"],
+  weight: ["300", "500", "600", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
+  adjustFontFallback: false,
 });
 
 const bricolageGrotesque = Bricolage_Grotesque({
   variable: "--font-bricolage-grotesque",
   subsets: ["latin"],
+  weight: ["700"],
+  display: "swap",
+  adjustFontFallback: false,
 });
 
 export const metadata: Metadata = {
-  title: "FEM - Weather App Hackaton",
+  title: "Frontend Mentor | Weather app Hackaton",
   description: "FrontendMentor weather app hackaton",
 };
 
