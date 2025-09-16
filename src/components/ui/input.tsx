6ff339b8ac5046b6a563dbc0de765c2023ccb664
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 04:48:43 +0000

    feat(input): update the input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,18 +7,25 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, ...props }, ref) => {
     const Icon = icon;
 
+    const id = React.useId();
+    const searchId = `search-${id}`;
+
     return (
       <div className="w-full relative">
         {Icon && (
-          <div className="absolute left-6 top-1/2 transform -translate-y-1/2">
-            <Icon size={18} className="text-neutral-200" />
-          </div>
+          <label
+            id={searchId}
+            className="absolute left-6 top-1/2 transform -translate-y-1/2"
+          >
+            <Icon size={20} className="text-neutral-200" />
+          </label>
         )}
 
         <input
           type={type}
+          id={searchId}
           className={cn(
-            "flex h-[3.5rem] w-full rounded-12  bg-neutral-800  px-6 text-fs-preset-5 ring-offset-background file:border-0 file:bg-neutral-800 file:text-fs-preset-5 file:font-medium placeholder:text-neutral-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-[3.5rem] text-neutral-0 w-full rounded-12  bg-neutral-800  px-6 text-fs-preset-5 ring-offset-background file:border-0 file:bg-neutral-800 file:text-fs-preset-5 file:font-medium placeholder:text-neutral-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
             icon ? "pl-15" : "",
             className,
           )}
