82e42ef5f3a74c222233fae7df72091d1ecde503
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 21:51:50 +0000

    feat: add the header component

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,13 @@
+import Title from "../common/Title";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col gap-16 justify-center items-center mt-16">
+      <Title level="h1" className="text-preset-2 ">
+        How&apos;s the sky looking today?
+      </Title>
+    </header>
+  );
+};
+
+export default Header;
