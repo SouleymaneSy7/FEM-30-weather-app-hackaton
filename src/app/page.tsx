14b72e0733fbd7e531b7f39ec655d1b15a4c68a0
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 04:37:46 +0000

    chore: update components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 export default function Home() {
-  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="m-5 flex flex-row  items-center gap-2 md:flex-row">
+      <Input placeholder="Search for a city" icon={Search} />
+      <Button>Button</Button>
+    </div>
+  );
 }
