b15e74f250df3a921002f945468e41fe43e86086
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 21:51:25 +0000

    feat: add the units parameters button component

---
+++ src/components/layouts/UnitsParameters.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Settings } from "lucide-react";
+
+const UnitsParameters = () => {
+  return (
+    <Select>
+      <SelectTrigger className="min-w-[90px] min-h-[43px] shadow-none border-none bg-neutral-800 text-neutral-0 lg:min-w-[120px]">
+        <Settings className="text-neutral-0" />
+        <SelectValue
+          placeholder="Units"
+          className="text-fs-preset-8 font-medium text-neutral-0"
+        />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="imperials">Switch to Imperial/Metric</SelectItem>
+
+        <SelectGroup>
+          <SelectLabel>Temperature</SelectLabel>
+          <SelectItem value="celsius">Celsius (°C)</SelectItem>
+          <SelectItem value="fahrenheit">Fahrenheit (°F)</SelectItem>
+        </SelectGroup>
+
+        <SelectGroup>
+          <SelectLabel>Wind Speed</SelectLabel>
+          <SelectItem value="km">km/h</SelectItem>
+          <SelectItem value="mph">mph</SelectItem>
+        </SelectGroup>
+
+        <SelectGroup>
+          <SelectLabel>Precipitation</SelectLabel>
+          <SelectItem value="mm">Millimeters (mm)</SelectItem>
+          <SelectItem value="in">Inches (in)</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default UnitsParameters;
