c9fa3c2aad34509827627ba9d726e991213645bb
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 02:31:14 +0000

    feat(button): add the button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-5xl bg-red-500 font-bold text-red-500 underline">
-      Hello world!
-    </h1>
-  );
+  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
 }
