dad24d880bcc07cecca519b239357918e50b90e9
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-16 21:53:13 +0000

    feat: update the homepage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import Header from "@/components/layouts/Header";
+import Navbar from "@/components/layouts/Navbar";
+
 export default function Home() {
   return (
-    <div className="m-5 flex flex-row  items-center gap-2 md:flex-row">
-      <Input placeholder="Search for a city" icon={Search} />
-      <Button>Button</Button>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
