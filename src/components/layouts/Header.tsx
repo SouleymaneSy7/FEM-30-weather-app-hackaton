88b4ddc87bf5991cd93d57b24b35ce8a87a4e28f
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-17 10:49:51 +0000

    feat: update the header component

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import Title from "../common/Title";
 const Header = () => {
   return (
     <header className="flex flex-col gap-16 justify-center items-center mt-16">
-      <Title level="h1" className="text-preset-2 ">
+      <Title level="h1" className="text-preset-2">
         How&apos;s the sky looking today?
       </Title>
     </header>
