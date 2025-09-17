7b4939b064b3050c404e0bc2ec5c75338a00f727
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-17 10:48:28 +0000

    feat(input): update the input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,42 +1,21 @@
-import * as React from "react";
+import * as React from "react"
 
-import { cn } from "@/lib/utils";
-import { InputProps } from "@/types";
+import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, icon, ...props }, ref) => {
-    const Icon = icon;
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
+      {...props}
+    />
+  )
+}
 
-    const id = React.useId();
-    const searchId = `search-${id}`;
-
-    return (
-      <div className="w-full relative">
-        {Icon && (
-          <label
-            id={searchId}
-            className="absolute left-6 top-1/2 transform -translate-y-1/2"
-          >
-            <Icon size={20} className="text-neutral-200" />
-          </label>
-        )}
-
-        <input
-          type={type}
-          id={searchId}
-          className={cn(
-            "flex h-[3.5rem] text-neutral-0 w-full rounded-12  bg-neutral-800  px-6 text-fs-preset-5 ring-offset-background file:border-0 file:bg-neutral-800 file:text-fs-preset-5 file:font-medium placeholder:text-neutral-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
-            icon ? "pl-15" : "",
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    );
-  },
-);
-
-Input.displayName = "Input";
-
-export { Input };
+export { Input }
