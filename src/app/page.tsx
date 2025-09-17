24dba9503ec79d12c23ab8601306f8bf9afe2728
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-17 15:58:09 +0000

    feat: add the main contents component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import * as React from "react";
+
 import Header from "@/components/layouts/Header";
 import Navbar from "@/components/layouts/Navbar";
+import Main from "@/components/layouts/Main";
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
       <Header />
-    </>
+      <Main />
+    </React.Fragment>
   );
 }
