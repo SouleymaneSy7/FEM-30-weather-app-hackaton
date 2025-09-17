380a2feb43b0143c764fbfa5052b7b646a5db3dd
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-17 14:23:55 +0000

    feat(input): update the input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,35 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { InputProps } from "@/types";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, icon, containerWidth, ...props }, ref) => {
+    const Icon = icon;
 
-export { Input }
+    return (
+      <div className={`relative w-full ${containerWidth}`}>
+        {Icon && (
+          <div className="absolute left-6 top-1/2 transform -translate-y-1/2">
+            <Icon size={20} className="text-muted-foreground" />
+          </div>
+        )}
+
+        <input
+          type={type}
+          className={cn(
+            "flex h-[3.5rem] w-full h-14 rounded-12  bg-card px-6 text-fs-preset-5 ring-offset-background file:border-0 file:bg-card file:text-fs-preset-5 file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+            icon ? "pl-15" : "",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export { Input };
