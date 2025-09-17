f5f3856488bf05a5e642c0dbbb5885e0c2da3360
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-17 10:48:09 +0000

    feat(button): update the button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,16 +5,16 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-12 text-fs-preset-5 font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
-        default: "bg-blue-500 text-primary-foreground hover:bg-blue-700",
-        secondary:
-          "bg-neutral-800 text-secondary-foreground hover:bg-neutral-700",
+        default:
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        secondary: "bg-card text-card-foreground hover:bg-card/80",
       },
       size: {
-        default: "w-[114px] h-[56px] px-4 py-5 has-[>svg]:px-3",
+        default: "w-full max-w-[114px] h-[56px] px-4 py-5",
         icon: "size-[43px]",
       },
     },
